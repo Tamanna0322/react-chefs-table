92c18473a7e3095ee1eca3f7ce449d3b7524c237
Tamanna0322 <tamanna0322@gmail.com>
2024-03-17 12:20:33 +0600

    dynamic card info

--- src/components/Header/Card/Card.jsx
+++ src/components/Header/Card/Card.jsx
@@ -1,18 +1,29 @@
 import PropTypes from 'prop-types';
+import { IoTimeOutline } from "react-icons/io5";
+import { AiTwotoneFire } from "react-icons/ai";
 
 const Card = ({ card }) => {
-     console.log(card);
+    //  console.log(card);
+     const {id, recipe_name, recipe_image, short_description, ingredients, preparing_time, calories} = card
+
     return (
         <div className='border-2 p-5 rounded-xl space-y-4'>
-            <img className='w-[350px] rounded-2xl' src="https://i.ibb.co/h9Vh047/spaghetti-seafood-with-tomato-sauce-decorated-with-beautiful-ingredients.jpg" alt="" />
-            <h3 className='text-xl font-bold'>Sphagetti Pasta</h3>
-            <p>Creamy and flavorful seafood risotto, perfect for cozy dinners. A vegetarian delight!</p>
+            <img className='w-[350px] h-[200px] rounded-2xl object-cover' src={recipe_image} alt="" />
+            <h3 className='text-xl font-bold'>{recipe_name}</h3>
+            <p>{short_description}</p>
             <hr />
-            <h3 className='text-xl font-bold'>Ingradients:</h3>
+            <h3 className='text-xl font-bold'>Ingradients: {ingredients.length}</h3>
+            {
+                ingredients.map((ingredient, idx) => <li key={idx}>{ingredient}</li>)
+            }
             <hr />
-            <div className='flex'>
-                <h3>preparing_time: </h3>
-                <h3>Calories: </h3>
+            <div className='flex space-x-8'>
+                <h3 className='flex items-center space-x-2'>
+                    <IoTimeOutline></IoTimeOutline> <span>{preparing_time}</span> minutes
+                </h3>
+                <h3 className='flex items-center space-x-2'>
+                   <AiTwotoneFire></AiTwotoneFire> <span>{calories}</span>calories
+                </h3>
             </div>
             <button className="btn bg-green-500 border-none lg:px-6 px-3 py-2 rounded-3xl font-bold">Want to Cook</button>
 
